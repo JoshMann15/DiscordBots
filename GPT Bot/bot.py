5f2d1bc2edf1f66cd05f235555ed41fb1436723b
Juice <JoshNahamkes@gmail.com>
2023-01-26 11:25:58 +1100

    uncluttered

--- GPT Bot/bot.py
+++ GPT Bot/bot.py
@@ -7,19 +7,14 @@ import os
 from pychatgpt import ChatGPT
 import time
 
-
-#For a more secure, we loaded the .env file and assign the token value to a variable 
 load_dotenv(".env")
 SessionID = os.getenv("SessionID")
 
-#Intents are permissions for the bot that are enabled based on the features necessary to run the bot.
 intents=discord.Intents.all()
 
-#Comman prefix is setup here, this is what you have to type to issue a command to the bot
 prefix = '$'
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-#Removed the help command to create a custom help guide
 bot.remove_command('help')
 
 def get_api():
@@ -41,8 +36,6 @@ def get_response_from_chatgpt(api, text):
       response = "Openai said: I'm so tired. Let me lie down for a few days. If you like, you can visit my home."
     return response
 
-#-----------------------------------------Moderation---------------------------------------------------------------#
-
 @bot.event
 async def on_command_error(context, error):
     if isinstance(error, commands.MissingRequiredArgument):
@@ -51,14 +44,12 @@ async def on_command_error(context, error):
         await context.send("Oh no! Looks like you Dont have the permissions for this command.")
     if isinstance(error, commands.MissingRole):
         await context.send("Oh no! Looks like you Dont have the roles for this command.")
-    #bot errors
+    
     if isinstance(error, commands.BotMissingPermissions):
         await context.send("Oh no! Looks like I Dont have the permissions for this command.")
     if isinstance(error, commands.BotMissingRole):
         await context.send("Oh no! Looks like I Dont have the roles for this command.")
     
-
-#|------------------COMMANDS------------------|   
 
 @bot.command()
 async def query(ctx, *args):
@@ -68,6 +59,4 @@ async def query(ctx, *args):
     line=line[:-1]
     await ctx.send(get_response_from_chatgpt(get_api(), line))
     
-
-#Run the bot
 bot.run(TOKEN)
--- OpenAI Image Bot/bot.py
+++ OpenAI Image Bot/bot.py
@@ -6,25 +6,21 @@ import asyncio
 import os
 import openai
 
-#For a more secure, we loaded the .env file and assign the token value to a variable 
 load_dotenv(".env")
 TOKEN = os.getenv("TOKEN")
 api_tokens = 0.27
 
 openai.api_key = os.getenv('api_key')
-#Intents are permissions for the bot that are enabled based on the features necessary to run the bot.
+
 intents=discord.Intents.all()
 
-#Comman prefix is setup here, this is what you have to type to issue a command to the bot
 prefix = '$'
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-#Removed the help command to create a custom help guide
+
 bot.remove_command('help')
 
-#------------------------------------------------Events------------------------------------------------------#
 
-#Basic Discord Bot Commands: Chat with your bot!
 @bot.command(name='hi')
 async def msg(ctx):
     if ctx.author == bot.user:
@@ -32,7 +28,6 @@ async def msg(ctx):
     else:
         await ctx.send("Hello there!")
 
-#-----------------------------------------Moderation---------------------------------------------------------------#
 
 @bot.event
 async def on_command_error(context, error):
@@ -42,14 +37,12 @@ async def on_command_error(context, error):
         await context.send("Oh no! Looks like you Dont have the permissions for this command.")
     if isinstance(error, commands.MissingRole):
         await context.send("Oh no! Looks like you Dont have the roles for this command.")
-    #bot errors
+    
     if isinstance(error, commands.BotMissingPermissions):
         await context.send("Oh no! Looks like I Dont have the permissions for this command.")
     if isinstance(error, commands.BotMissingRole):
         await context.send("Oh no! Looks like I Dont have the roles for this command.")
     
-
-#|------------------COMMANDS------------------|
 
 @bot.command()
 async def help(message):
@@ -84,26 +77,22 @@ async def create_image(message, *args):
         embed.set_image(url=image_url)
         print("Img Embed Loaded: "+image_url);
         
-        #print(line)
+        
         
         await message.channel.send(embed=embed)
-        #await ctx.send(image_url + "\nIMAGES USED: " + str((api_tokens/18)*100) + "%")
+        
     except Exception as e:
         print(e)
     
 
-
-
-    
-
 @bot.command()
-#Checks whether the user has the correct permissions when this command is issued
+
 @commands.has_permissions(manage_messages=True)
 async def clear(context, amount=5):
     await context.channel.purge(limit=amount+1)
 
 print("Command Started!");
-#embed = discord.Embed(title="Image with prompt: "+message, description="A discord bot built with python used to interactive with the stable diffusion API")
+
 
 '''
 print("Embed Started!");
@@ -114,8 +103,8 @@ response = openai.Image.create(
 )
 print("Response Created!");
 image_url = response['data'][0]['url']
-#embed.setImage(image_url)
+
 print("Img Embed Loaded: "+image_url);
 '''
-#Run the bot
+
 bot.run(TOKEN)
